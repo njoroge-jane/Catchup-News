338309346dc5175d94d7490178bcce1eda526db3
Jane Njoroge <jane.njoroge@student.moringaschool.com>
2022-02-01 02:31:07 +0300

    create an article class model

--- app/models.py
+++ app/models.py
@@ -7,3 +7,14 @@ class Source:
         self.url = url
         self.country = country
 
+class Source:
+
+    def __init__(self,id,title,image,description,url,author,publishedAt):
+        self.id =id
+        self.title = title
+        self.image = image
+        self.url = url
+        self.description = description
+        self.author = author
+        self.publishedAt = publishedAt
+
