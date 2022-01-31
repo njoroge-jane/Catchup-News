2e77f790d3c52dac287799ee91e07e9df5737a7b
Jane Njoroge <jane.njoroge@student.moringaschool.com>
2022-02-01 02:11:16 +0300

    add indentation

--- app/__init__.py
+++ app/__init__.py
@@ -4,14 +4,15 @@ from config import config_options
 
 bootstrap = Bootstrap()
 
+
 def create_app(config_name):
-  app=Flask(__name__)
-  from .main import main as main_blueprint
-  app.register_blueprint(main_blueprint)
-  app.config.from_object(config_options[config_name])
+    app = Flask(__name__)
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+    app.config.from_object(config_options[config_name])
 
-  from .request import configure_request
-  configure_request(app)
+    from .request import configure_request
+    configure_request(app)
 
-  bootstrap.init_app(app)
-  return app
+    bootstrap.init_app(app)
+    return app
